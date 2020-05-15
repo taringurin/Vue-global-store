fbaa554e063cd48390a852651373bbe242702baf
Mr.Tarin Gurin <tarin_g@dhas.com>
2020-05-15 14:35:13 +0700

    Fixed value call funtions

--- STORE_CONST.js
+++ STORE_CONST.js
@@ -1,30 +1,32 @@
 const STORE = {
-    getterNames: {
-      userDetialLoginStatus: 'user/userDetail/loginStatus',
-    },
-    actionNames: {
-      userDetialLogin: 'user/userDetail/login',
-      userDetialLogout: 'user/userDetail/logout'
-    }
+  getterNames: {
+    userDetialLoginStatus: 'user/userDetail/loginStatus',
+  },
+  actionNames: {
+    userDetialLogin: 'user/userDetail/login'
   }
+}
   
-  export default STORE
-  
-  // >>> example <<<
-  // === Js/Ts file ===
-  // import STORE_CONST from '../store-const.js'
-  // const userLogged = await store.getters[STORE_CONST.getterNames.userDetialLoginStatus]
-  
-  // === Vue file ===
-  // 1.) Import file
-  // import STORE_CONST from '~/store-const'
-  // 2.) Declaration
-  // data () {
-  //   return {
-  //     actions: {
-  //       login: STORE_CONST.actionNames.userDetialLogin
-  //     }
-  //   }
-  // }
-  // 3.) Get user
-  // const userLogged = this.$store.dispatch(this.actions.userDetialLoginStatus)
+export default STORE
+
+// >>> example <<<
+// === Js/Ts file ===
+// import STORE_CONST from '../store-const.js'
+// const userLogged = await store.getters[STORE_CONST.getterNames.userDetialLoginStatus]
+
+// === Vue file ===
+// 1.) Import file
+// import STORE_CONST from '~/store-const'
+// 2.) Declaration
+// data () {
+//   return {
+//     actions: {
+//       login: STORE_CONST.actionNames.userDetialLogin
+//     }
+//   }
+// }
+// 3.) Get user
+// const login = await this.$store.dispatch(this.actions.login, {
+//     username: this.username,
+//     password: this.password
+// })
